bd63c22c5c517f99048152a335392c989f37eeea
陈不不 <72036853+2833844911@users.noreply.github.com>
2023-06-29 01:31:44 +0800

    Update train.py
    准确概 更加高

--- train.py
+++ train.py
@@ -6,15 +6,14 @@ date: 2023-04-18
 
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, Dataset
-from torchvision.datasets import ImageFolder
+
 import torch
 import cv2
 import random
 import os
 import torch.optim as optim
 import torch.nn as nn
-import numpy as np
-import torch.nn.functional as F
+
 import torchvision.models as models
 from tqdm import tqdm
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
@@ -37,14 +36,14 @@ class CustomResNet50(nn.Module):
         self.flatten = nn.Flatten()
         self.fc = nn.Linear(2048, num_classes)
         # self.fc = nn.Linear(8192, num_classes)
-        # self.sfm = nn.Softmax(1)
+        self.sfm = nn.Sigmoid()
 
     def forward(self, x):
         # print(x.shape)
         x = self.resnet_layers(x)
         x = self.flatten(x)
         x = self.fc(x)
-        # x = self.sfm(x)
+        x = self.sfm(x)
         return x
 
 resnet = CustomResNet50(num_classes=num_classes)
@@ -77,21 +76,21 @@ def add_gaussian_noise(image, mean=0, sigma=25):
     :return: 带有高斯噪声的图像
     """
     # 将图像转换为浮点数，以防止整数溢出
-    image = np.array(image, dtype=np.float64)
+    # noisy_image = np.array(image, dtype=np.float64)
 
     # 生成与图像尺寸相同的高斯噪声
-    noise = np.random.normal(mean, sigma, image.shape)
-
-    # 将噪声添加到图像
-    noisy_image = image + noise
+    # noise = np.random.normal(mean, sigma, image.shape)
+    #
+    # # 将噪声添加到图像
+    # noisy_image = image + noise
 
     # 确保结果在0到255范围内
-    noisy_image = np.clip(noisy_image, 0, 255)
+    # noisy_image = np.clip(noisy_image, 0, 255)
 
     # 将图像类型转换回原始类型
-    noisy_image = noisy_image.astype(np.uint8)
+    # noisy_image = noisy_image.astype(np.uint8)
 
-    return noisy_image
+    return image
 class data_Dataset(Dataset):
     def __init__(self, data) -> None:
         super().__init__()
@@ -100,9 +99,6 @@ class data_Dataset(Dataset):
     def __getitem__(self, index):
         tmfile = self.data[index]
         data = cv2.imread(tmfile)
-        data = crop_max_square(data)
-        data = add_gaussian_noise(data)
-        # cv2截取图片最大正方形
 
 
 
@@ -115,22 +111,9 @@ class data_Dataset(Dataset):
         M = cv2.getRotationMatrix2D(center, angle, scale)
 
         # 应用旋转矩阵
-        rotated = cv2.warpAffine(data, M, (data.shape[1], data.shape[0]),borderValue=(255,255,255))
+        croppedcj = cv2.warpAffine(data, M, (data.shape[1], data.shape[0]),borderValue=(255,255,255))
         
-        # 中心圆圈裁剪
-        mask = np.zeros((rotated.shape[0], rotated.shape[1]), dtype=np.uint8)
-        x, y = np.meshgrid(np.arange(rotated.shape[1]), np.arange(rotated.shape[0]))
-        dist = np.sqrt((x - center[0])**2 + (y - center[1])**2)
-        mask[dist <= rotated.shape[0] // 2] = 255
-        cropped = cv2.bitwise_and(rotated, rotated, mask=mask)
-        croppedcj = cropped[:, :]
 
-        # 显示旋转后的图像
-        # cv2.imshow('Rotated Image', croppedcj)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-        # 保存图片 
-        cv2.imwrite('./test/{}_{}.jpg'.format(index, angle), croppedcj)
 
         transform = transforms.Compose([
             transforms.ToTensor()
@@ -151,7 +134,7 @@ class data_Dataset(Dataset):
 train_dataset = data_Dataset(tranfiles)
 
 test_dataset = data_Dataset(textfiles)
-batch_size = 4
+batch_size = 10
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
@@ -161,21 +144,32 @@ class CustomClassificationLoss(nn.Module):
         super(CustomClassificationLoss, self).__init__()
         self.num_classes = num_classes
         self.alpha = alpha
-        self.cross_entropy = nn.CrossEntropyLoss()
+
 
     def forward(self, logits, targets):
-        # 计算交叉熵损失
-        ce_loss = self.cross_entropy(logits, targets)
 
-        # 计算距离损失
-        probabilities = F.softmax(logits, dim=1)
-        max_prob, _ = torch.max(probabilities, dim=1)
-        daloss = torch.abs(_-targets)/360
-        regularization = torch.mean(daloss)
-        xiaoss = self.alpha * regularization
+        ce_loss = 0
+        for i in range(logits.shape[0]):
+            wz = targets[i].item()
+            kj = wz
+            lossOne = (1-logits[i][wz])
+            for r in range(1, 180):
+                wz -= 1
+                if wz <0:
+                    wz = 359
+                lossOne += torch.abs((0.98 ** r) - logits[i][wz])
+
+            for r in range(1, 180):
+                kj += 1
+                if kj >359:
+                    kj = 0
+                lossOne += torch.abs((0.98 ** r) - logits[i][kj])
+            ce_loss += lossOne
+
+
         # 组合损失
-        loss = ce_loss + xiaoss
-        return loss, xiaoss.item(), ce_loss.item()
+        loss = ce_loss/targets.shape[0]
+        return loss, 0, loss.item()
 
 # model = LeNet5()
 criterion = CustomClassificationLoss(360)
